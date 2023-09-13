890dbd9f3349392c8051fe5b09e67228158e4da2
usuario <alu0101548197.com>
2023-09-13 19:55:15 +0000

    mej

---
+++ practicas/p01_single_grades/p01_single_grades.h
@@ -0,0 +1,16 @@
+// Universidad de La Laguna
+// Escuela Superior de Ingenier´ıa y Tecnolog´ıa
+// Grado en Ingenier´ıa Inform´atica
+// Asignatura: Computabilidad y Algoritmia
+// Curso: 2º
+// Pr´actica 1: Contenedores asociativos
+// Autor: Nombre y Apellidos
+// Correo: aluXXXXX@ull.edu.es
+// Fecha: 12/09/2023
+// Archivo cya-P01-single-grades.cc: programa cliente.
+// Contiene la funci´on main del proyecto que usa las clases X e Y
+// para ... (indicar brevemente el objetivo)
+// Referencias:
+// Enlaces de inter´es
+// Historial de revisiones
+// 12/09/2023 - Creaci´on (primera versi´on) del c´odigo
